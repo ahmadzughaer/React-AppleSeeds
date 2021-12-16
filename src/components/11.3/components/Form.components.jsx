031692696ba3141cc09808e65a995767a50c7f11
ahmadzughaer <ahmadzughaer@gmail.com>
2021-12-16 09:06:45 +0200

    more components added

--- src/components/11.3/components/Form.components.jsx
+++ src/components/11.3/components/Form.components.jsx
@@ -5,8 +5,13 @@ class From extends React.Component {
     render() {
         return (
             <div>
-                
+                <label>First name</label>
+                <input/>
+                <label>last name</label>
+                <input/>
             </div>
         )
     }
 }
+
+export default From;
